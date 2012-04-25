9e234a854554f7141efdfb246c0cecc5de777c7c
Louis Chatriot <louis.chatriot@gmail.com>
2012-04-25 12:12:09 +0200

    Make piece go down one step ok

--- js/tetris.js
+++ js/tetris.js
@@ -1,7 +1,7 @@
 var containerId = '#gameContainer'
   , displayBox = $(containerId)
   , matrixWidth = 10, matrixHeight = 20     // Measured in number of minos
-  , minoWidth = 15, minoHeight = 15         // Measured in pixels
+  , minoWidth = 8, minoHeight = 8         // Measured in pixels
   , matrixState = []
   , score, lineCount, currentLevel
   , i, j;
@@ -23,14 +23,16 @@ var initializeMatrix = function() {
 }
 
 
+// Calculate and set a mino's coordinates
 var getLeft = function(x) { return minoWidth * x; }
 var getTop = function(y) { return minoHeight * y; }
-
 var placeMino = function(mino, place) {
   mino.css('left', getLeft( currentPiece.centerX + place.x ));
   mino.css('top', getTop( currentPiece.centerY + place.y ));
 }
 
+
+// After last piece was placed, create a new one on top of the matrix
 var createNewPiece = function() {
   var temp, i;
 
@@ -46,11 +48,12 @@ var createNewPiece = function() {
     temp.css('height', minoHeight);
     placeMino(temp, pieces[currentPiece.type][i]);
     displayBox.append(temp);
-    currentPiece.minos.push(temp);
+    currentPiece.minos.push(temp);    // The minos array is in the same order as pieces[currentPiece]
   }
 }
 
 
+// Returns true if the piece hits another piece or the bottom
 var cantMoveAnymore = function() {
   var result = false
     , movingPiece = pieces[currentPiece.type]
@@ -60,27 +63,37 @@ var cantMoveAnymore = function() {
   for (i = 0; i < movingPiece.length; i += 1) {
     theX = currentPiece.centerX + movingPiece[i].x;
     theY = currentPiece.centerY + movingPiece[i].y;
-    if ( (matrixState[theX][theY + 1] !== null) || theY >= matrixHeight ) { result = true; }   // If the piece hits another piece or the bottom
+    if ( (matrixState[theX][theY + 1] !== null) || theY >= matrixHeight ) { result = true; }
   }
 
   return result;
 }
 
 
-var makePieceMoveOneStep = function() {
-  var i;
+// Make piece move one step towards the bottom
+var makeCurrentPieceMoveOneStep = function() {
+  var movingPiece = pieces[currentPiece.type]
+    , theX, theY
+    , i;
 
   currentPiece.centerY += 1;
 
-  alert('ewfdwe wer w');
+  for (i = 0; i < movingPiece.length; i += 1) {
+    // The minos array is in the same order as movingPiece
+    placeMino(currentPiece.minos[i], movingPiece[i]);
+  }
 }
 
 
+var rotateCurrentPieceLeft = function() {
+
+
+}
 
 initializeMatrix();
 createNewPiece();
 
-$('#theButton').on('click', makePieceMoveOneStep);
+$('#theButton').on('click', makeCurrentPieceMoveOneStep);
 
 
 
