7299af614129c711f0bb70b23b6f27d060716469
Louis Chatriot <louis.chatriot@gmail.com>
2012-04-30 16:51:45 +0200

    Added iterator on piece

--- js/tetris.js
+++ js/tetris.js
@@ -1,12 +1,13 @@
-var containerId = '#gameContainer'
-  , displayBox = $(containerId)
+var containerId = '#gameContainer', scoreContainerId = '#scoreContainer', lineCountContainerId = '#lineCountContainer', levelContainerId = '#levelContainer'
   , matrixWidth = 10, matrixHeight = 20     // Measured in number of minos
-  , minoWidth = 8, minoHeight = 8         // Measured in pixels
+  , minoWidth = 8, minoHeight = 8           // Measured in pixels
+  , leftZero = 0, topZero = 0;
+
+var displayBox = $(containerId)
   , matrixState = []
   , score = 0, lineCount = 0, currentLevel = 1
-  , scorePerNumberOfLines = [0, 80, 200, 400, 1000]
-  , i, j
-  , intervalId, gamePaused = false;
+  , intervalId, gamePaused = false
+  , i, j;
 
 var pieces = [ [{x: 0, y: 0}, {x: -1, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]        // T
              , [{x: 0, y: 0}, {x: -1, y: 0}, {x: 2, y: 0}, {x: 1, y: 0}]        // Bar
@@ -17,11 +18,12 @@ var pieces = [ [{x: 0, y: 0}, {x: -1, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]        
              , [{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}] ]      // Square
 
   , squareType = 6    // Used to prevent the square from rotating
-  , currentPiece = {type: 0, centerX: 0, centerY: 0, rotation: [], minos: []};
+  , currentPiece = {type: 0, centerX: 0, centerY: 0, rotation: [], minos: []}
+  , scorePerNumberOfLines = [0, 80, 200, 400, 1000];
 
 
-// Initialize matrix status to only nulls (empty start state)
-var initializeMatrix = function() {
+// Initialize matrix status to only nulls (empty start state) and place HUD items
+var initializeGame = function() {
   var temp;
   for (i = 0; i < matrixWidth; i += 1) {
     temp = [];
@@ -31,12 +33,15 @@ var initializeMatrix = function() {
 
   displayBox.css('width', matrixWidth * minoWidth);
   displayBox.css('height', matrixHeight * minoHeight);
+  displayBox.css('left', getLeft(0));
+  displayBox.css('top', getTop(0));
+
 }
 
 
 // Calculate and set a mino's coordinates
-var getLeft = function(x) { return minoWidth * x; }
-var getTop = function(y) { return minoHeight * y; }
+var getLeft = function(x) { return leftZero + (minoWidth * x); }
+var getTop = function(y) { return topZero + (minoHeight * y); }
 
 var getActualXCoord = function(place) { return currentPiece.centerX + place.x; }
 var getActualYCoord = function(place) { return currentPiece.centerY + place.y; }
@@ -49,29 +54,21 @@ var placeMino = function(mino, place) {
 
 // After last piece was placed, create a new one on top of the matrix
 var createNewPiece = function() {
-  var temp, i;
+  var temp;
 
   // Re-initialize currentPiece
-  currentPiece.type = Math.floor(Math.random() * pieces.length);     // TODO randomize piece choice
+  currentPiece.type = Math.floor(Math.random() * pieces.length);
   currentPiece.centerX = Math.floor(matrixWidth / 2);
   currentPiece.centerY = 1;
   currentPiece.minos = [];
   currentPiece.rotation = [];
 
-  // Just for fun, TODO remove
-  var color = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 99) + ', ' + Math.floor(Math.random() * 99) +')';
-
   for (i = 0; i < pieces[currentPiece.type].length; i += 1) {
     temp = $(document.createElement('div'));
     temp.attr('class', 'piece' + currentPiece.type);
-
-    //temp.css('border-style', 'solid');
-    //temp.css('border-width', 'thin');
-    //temp.css('border-color', '#ffffff');
-
     temp.css('width', minoWidth);
     temp.css('height', minoHeight);
-    //temp.css('background-color', color);
+
     placeMino(temp, pieces[currentPiece.type][i]);
     displayBox.append(temp);
     currentPiece.minos.push(temp);    // The minos array is in the same order as currentPiece.rotation
@@ -80,25 +77,48 @@ var createNewPiece = function() {
 }
 
 
-// Returns true if the piece hits another piece or the bottom
-var currentPieceCantMoveAnymore = function() {
-  var result = false
-    , theX, theY
-    , i;
+var iterateOnCurrentPiece = function(fn) {
+  var theX, theY;
 
   for (i = 0; i < currentPiece.rotation.length; i += 1) {
     theX = getActualXCoord( currentPiece.rotation[i] );
     theY = getActualYCoord( currentPiece.rotation[i] );
-
-    if ( matrixState[theX][theY] !== null) {
-      result = true;
-    }
-    if (theY >= matrixHeight - 1) {
-      result = true;
-    } else if (matrixState[theX][theY + 1] !== null) {
-      result = true;
-    }
+    fn(theX, theY);
   }
+}
+
+
+// Returns true if the piece hits another piece or the bottom
+var currentPieceCantMoveAnymore = function() {
+  var result = false
+    //, theX, theY
+    //, i;
+
+  iterateOnCurrentPiece(function(xc, yc) { 
+    if ( matrixState[xc][yc] !== null) {
+      result = true;
+    }
+    if (yc >= matrixHeight - 1) {
+      result = true;
+    } else if (matrixState[xc][yc + 1] !== null) {
+      result = true;
+    }
+  });
+
+
+  //for (i = 0; i < currentPiece.rotation.length; i += 1) {
+    //theX = getActualXCoord( currentPiece.rotation[i] );
+    //theY = getActualYCoord( currentPiece.rotation[i] );
+
+    //if ( matrixState[theX][theY] !== null) {
+      //result = true;
+    //}
+    //if (theY >= matrixHeight - 1) {
+      //result = true;
+    //} else if (matrixState[theX][theY + 1] !== null) {
+      //result = true;
+    //}
+  //}
 
   return result;
 }
@@ -238,16 +258,6 @@ var rotateCurrentPieceLeft = function() {
 }
 
 
-var rotateCurrentPieceRight = function() {
-  var i, temp;
-
-  for (i = 0; i < currentPiece.rotation.length; i += 1) {
-    temp = currentPiece.rotation[i].y;
-    currentPiece.rotation[i].y = currentPiece.rotation[i].x;
-    currentPiece.rotation[i].x = - temp;
-  }
-}
-
 var blockCurrentPiece = function() {
   var theX, theY
     , i;
@@ -330,7 +340,7 @@ var updateHUD = function(linesMade) {
 }
 
 
-initializeMatrix();
+initializeGame();
 createNewPiece();
 
 
